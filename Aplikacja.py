abc3f9141cf825dc7eccca365ff1f1720b60fd1f
Cristobal <krystofer02@gmail.com>
2021-12-13 23:23:48 +0100

    little changes

--- Aplikacja.py
+++ Aplikacja.py
@@ -1,16 +1,22 @@
 # imports
+import kivy
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.app import App
 from kivy.utils import interpolate
 
+# wymagana wersja frameworka kivy.
+kivy.require('2.0.0')
+
 # poniższy import nie istnieje w virtualenv
 # from scipy import interpolate 
+
+import AppMenu
+
 
 class CustomButton(Button):
     def on_press(self):
         pass
-
 
 class Aplikacja(App):
     pass
