77775a1786d4d2263c0025ee0ce231f0af4ce5c7
Krystofer Lando <krystofer02@gmail.com>
2022-11-11 18:06:17 +0100

    Delete circular import

--- audio.py
+++ audio.py
@@ -1,9 +1,8 @@
 #  Copyright (c) 2022.
 #  This file is part of "System Filtracji Cyfrowej", which is released under GPLv2 license.
 #  Created by Krzysztof Kłapyta.
-from filters.filter_interface import IFilter
-from scipy.io import wavfile
 from kivy.logger import Logger
+from scipy.io import wavfile
 
 
 def load_audio_data(path):
@@ -20,7 +19,7 @@ def save_audio_file(path, data, sample_rate):
     wavfile.write(path, sample_rate, data)
 
 
-def process_samples(data, filter: IFilter = None):
+def process_samples(data, filter = None):
     if filter is None:
         processed_samples = data
     else:
