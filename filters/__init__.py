ae68d10167f34e4f9257d0aeea9639735fda79ca
Krzysztof <krystofer02@gmail.com>
2024-05-31 22:54:14 +0200

    Closes #5; Add loop for loading modules from filters directory with checking mro

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,25 +1,39 @@
 #  Copyright (c) 2022.
 #  This file is part of "System Filtracji Cyfrowej", which is released under GPLv2 license.
 #  Created by Krzysztof Kłapyta.
+import inspect
+import sys, os
+import importlib
+import logging
 
 from . import filter
 
-# import list of created filters.
-from .test_filter_fir_1 import TestFilterFIR
-from .test_filter_iir import TestFilterIIR
-from .bandpass_butterworth import BandpassButterworthFilter
+# sys.path.append('filters')
 
+log = logging.getLogger()
+log.setLevel(logging.INFO)
 
-# list for possibility of iterate by classes
-FILTER_LIST = [
-    TestFilterFIR,
-    TestFilterIIR,
-    BandpassButterworthFilter
-]
+FILTER_LIST = []
 
-# __all__ = [
-#     'FILTER_LIST',
-#     'TestFilterFIR',
-#     'TestFilterIIR',
-#     'BandpassButterworthFilter'
-# ]
+try:
+    for e in os.scandir('filters'):
+        if e.name == 'filter.py' or e.name == '__init__.py':
+            continue
+        if e.name.endswith('.py'):
+            name_module = 'filters.' + e.name.rstrip('.py')
+            module = importlib.import_module(name_module)
+            for d in dir(module):
+                obj_from_module = getattr(module, d)
+                try:
+                    # checking IFilter exists in MRO of class by duck typing.
+                    not_the_IFilter_itself = obj_from_module is not filter.IFilter 
+                    isclass = inspect.isclass(obj_from_module)
+                    if isclass and not_the_IFilter_itself:
+                        if issubclass(obj_from_module, filter.IFilter):
+                            FILTER_LIST.append(obj_from_module)
+                except AttributeError:
+                    log.info("There was not an mro method for object. Not a class.")
+                    continue  # this was not class ;)
+except Exception as e:
+    print(e)
+    raise
